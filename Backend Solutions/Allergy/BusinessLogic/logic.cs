ddc42d1c0aa204c1fa8f9053528701e50cce5c18
Sai Krishna <saikrishnahsr0@gmail.com>
2023-03-14 09:03:43 +0530

    Allergy DB Architecture done

---
+++ Backend Solutions/Allergy/BusinessLogic/logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace BusinessLogic
+{
+    internal class logic : Ilogic
+    {
+        public Allergy addPatientAllergy(Guid patientId, string AllergyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Allergy> GetAllAllergies(Guid patientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
