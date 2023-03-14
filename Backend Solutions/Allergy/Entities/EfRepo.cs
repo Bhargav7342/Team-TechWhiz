46ff9d46d3eaf82dfa1e67c2c025336900c212dd
Sai Krishna <saikrishnahsr0@gmail.com>
2023-03-14 18:29:51 +0530

    allergy db API s completed

--- Backend Solutions/Allergy/Entities/EfRepo.cs
+++ Backend Solutions/Allergy/Entities/EfRepo.cs
@@ -1,7 +1,25 @@
-﻿namespace Entities
-{
-    public class EfRepo
-    {
+﻿using DataEntities;
+using DataEntities.Entities;
 
+namespace Entities
+{
+    public class EfRepo : IEfRepo
+    {
+        private readonly AllergyDContext context;
+        public EfRepo(AllergyDContext _context)
+        {
+            context = _context;
+        }
+        public Allergy addPatientAllergy(Allergy allergy)
+        {
+            context.Allergies.Add(allergy);
+            context.SaveChanges();
+            return allergy;
+        }
+
+        public List<Allergy> GetAllAllergies(Guid patientId)
+        {
+            return context.Allergies.Where(x => x.PatientId == patientId).ToList();
+        }
     }
 }
