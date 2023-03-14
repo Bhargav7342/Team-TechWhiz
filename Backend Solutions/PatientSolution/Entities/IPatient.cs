0f591189abe19393224dc30d52df7bee24caa6a5
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-03-14 09:01:48 +0530

    Patient Services Done

---
+++ Backend Solutions/PatientSolution/Entities/IPatient.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEntities
+{
+    public interface IPatient
+    {
+        Patient RegisterPatient(Patient patient);
+        Patient UpdatePatient(Patient patient);
+        Patient DeletePatient(string email);
+        List<Patient> GetAllPatient();
+    }
+}
---
+++ Backend Solutions/PatientSolution/Services/Controllers/PatientController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Patient_Logic;
+
+namespace Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        IPatientLogic patientlogic;
+        public PatientController(IPatientLogic _patientlogic)
+        {
+            patientlogic = _patientlogic;
+        }
+
+        [HttpPost("Register_Patient")]
+        public IActionResult RegisterPat([FromBody]Patient patient)
+        {
+            var p = patientlogic.AddPatient(patient);
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            else
+                return BadRequest("Sorry...Something Went Wrong");
+        }
+
+        [HttpGet("SignIn_Patient")]
+        public IActionResult SignInPatient([FromHeader]string email, [FromHeader]string pass)
+        {
+            var p=patientlogic.LoginPatient(email, pass);
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            else
+                return BadRequest("Sorry Not Found");
+        }
+
+        [HttpDelete("Delete_Patient")]
+
+        public IActionResult Delete([FromHeader]string email)
+        {
+            var p=patientlogic.DeletePatient(email);
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            else
+                return BadRequest("Not Found");
+        }
+
+        [HttpPut("Update_Patient")]
+
+        public IActionResult Update([FromHeader]string email, [FromBody]Patient patient)
+        {
+            patientlogic.UpdatePatient(email, patient);
+            return Ok(patient);
+        }
+    }
+}
